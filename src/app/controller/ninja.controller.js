4d063095506cf28e59e45861421877385ed53b9e
dbertella <daniele.bertella@gmail.com>
2015-11-19 18:02:24 +0100

    add lesson 2 final

--- src/app/controller/ninja.controller.js
+++ src/app/controller/ninja.controller.js
@@ -1,37 +1,40 @@
 'use strict';
 
 angular.module('handlingNinja')
-.controller('ninjaCtrl', function($scope, Ninja, _) {
+.controller('ninjaCtrl', function($scope, $location, Ninja, _) {
 
-  $scope.ninjas = [];
-
+  $scope.ninjas = [
+    {
+      _id: '001',
+      name: 'daniele',
+      age: '30'
+    },
+    {
+      _id: '002',
+      name: 'gian',
+      age: '28'
+    }
+  ];
   $scope.loadNinja = function(){
-      $scope.ninjas = Ninja.query();
+    $scope.ninjas = Ninja.query();
   };
 
   $scope.newNinja = {name: null, age: null};
 
   $scope.createNinja = function(){
-      var ninja = new Ninja($scope.newNinja);
-
-      ninja.$save().then(function(res){
-          $scope.newNinja = {name: null, age: null};
-          $scope.ninjas.push(new Ninja(res));
-      });
-  };
-
-  $scope.getNinja = function(_id){
-      Ninja.get({_id: _id}).$promise
-      .then(function(ninja){
-          $scope.ninja = ninja;
-          $scope.detail = true;
-      });
+    var ninja = new Ninja($scope.newNinja);
+    ninja.$save()
+    .then(function(res){
+      $scope.newNinja = {name: null, age: null};
+      $scope.ninjas.push(new Ninja(res));
+    });
   };
 
   $scope.deleteNinja = function(ninja){
-      ninja.$remove().then(function(res){
-          _.remove($scope.ninjas, {_id: ninja._id});
-      });
+    ninja.$remove()
+    .then(function(){
+      _.remove($scope.ninjas, {_id: ninja._id});
+    });
   };
 
   // $scope.loadNinja();
--- src/app/controller/ninja_update.controller.js
+++ src/app/controller/ninja_update.controller.js
@@ -1,12 +1,17 @@
 'use strict';
 
 angular.module('handlingNinja')
-.controller('ninjaUpdateCtrl', function($scope, $location) {
+.controller('ninjaUpdateCtrl', function($scope, $location, $route, Ninja) {
   console.log('ninjaUpdateCtrl');
+  Ninja.get({_id: $route.current.params.id}).$promise
+  .then(function(ninja){
+    $scope.ninja = ninja;
+  });
+
   $scope.updateNinja = function(){
-      $scope.ninja.$save().then(function(res){
-          $scope.detail = null;
-          $scope.loadNinja();
-      });
+    $scope.ninja.$save()
+    .then(function() {
+      $location.url('/');
+    });
   };
 });
