a1cb88f78f1c3def404a1eddc4073bdc8eee32f2
dbertella <daniele.bertella@gmail.com>
2015-11-23 12:32:21 +0100

    add local server

--- src/app/service/ninja.service.js
+++ src/app/service/ninja.service.js
@@ -1,6 +1,6 @@
 angular.module('handlingNinja')
 .service('Ninja', function($resource){
-  var Ninja = $resource('http://localhost:3001/api/ninja/:_id', {
+  var Ninja = $resource('http://192.168.6.27:3001/api/ninja/:_id', {
     _id: '@_id'
   }, {
     count: {method: 'GET', isArray: true, params: {count: true}}
