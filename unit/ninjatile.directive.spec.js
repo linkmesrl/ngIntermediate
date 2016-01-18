a101ee8c12897f5504fdd9ccfd33097c1e661305
dbertella <daniele.bertella@gmail.com>
2016-01-18 11:53:58 +0100

    fix ninjatile spec

--- unit/ninjatile.directive.spec.js
+++ unit/ninjatile.directive.spec.js
@@ -2,46 +2,38 @@
 
 describe('The ninja-tile Directive', function () {
 
-  var mockUserResource, $httpBackend, element, scope, isolatedScope, compile;
-  var ninja = {
-      "age": 55,
-      "name": "Daniele",
-      "_id": "5693fa5af6976c704e00005a",
-      "__v": 0
-    };
-  
+  var element;
+  var scope;
+
   // injecting main module
   beforeEach(module('handlingNinja'));
   beforeEach(module('templates'));
 
   beforeEach(function() {
+    var ninja = {
+      'age': 55,
+      'name': 'Daniele',
+      '_id': '5693fa5af6976c704e00005a',
+      '__v': 0
+    };
     inject(function ($compile, $rootScope) {
-       
-      compile = $compile;
-       
       scope = $rootScope.$new();
-       
       scope.ninja = ninja;
+      element = $compile(angular.element('<ninja-tile ninja="ninja"></ninja-tile>'))(scope);
+      scope.$apply();
     });
-    element = getCompiledElement();
   });
-
-   function getCompiledElement(){
-     var compiledDirective = compile(angular.element('<ninja-tile ninja="ninja"></ninja-tile>'))(scope);
-     scope.$digest();
-     return compiledDirective;
-   }
-
-  it('Ninja on isolated scope should have age equal to 55', function () {
-      expect(scope.ninja.age).toEqual(55);
+  it('Ninja on isolated scope should be two-way bound', function(){
+    var isolatedScope = element.isolateScope();
+    isolatedScope.nt.ninja.age = 25;
+    expect(scope.ninja.age).toEqual(25);
+    scope.$apply();
+    expect(parseInt(element.find('.ninja-age').text().trim())).toEqual(25);
   });
-
   it('It should contains Daniele', function () {
-      expect( element.find('.ninja-name').text().trim() ).toEqual('Daniele');
+    expect(element.find('.ninja-name').text().trim()).toEqual('Daniele');
   });
-
   it('It should have age 55', function () {
-      expect( parseInt( element.find('.ninja-age').text().trim() ) ).toEqual(55);
+    expect(parseInt(element.find('.ninja-age').text().trim())).toEqual(55);
   });
-
 });
